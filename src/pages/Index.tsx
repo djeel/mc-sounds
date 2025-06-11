864ba6f6fa3c44793f298de612da3d429730a442
djeel <49989313+djeel@users.noreply.github.com>
2025-06-11 02:32:16 +0200

    refactor: Simplify key generation in main content for improved performance

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -383,7 +383,7 @@ const Index: React.FC = () => {
       {/* Main content */}
       <main ref={mainContentRef} className="container mx-auto px-4 py-6">
         <div
-          key={activeCategory + '-' + deferredSearchTerm + '-' + favorites.size}
+          key={activeCategory + '-' + deferredSearchTerm}
           className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
           role="tabpanel"
           id={`panel-${activeCategory}`}
